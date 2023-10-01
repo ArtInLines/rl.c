bb813edc361a7a617d3de5079b94dd270f13778e
ArtInLines <linesinart@gmail.com>
2023-10-01 16:23:20 +0200

    fix: include-paths

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ set CFLAGS=-Wall -Wextra -Wimplicit -Wpedantic -Wno-unused-function -std=c99
 set VCPKG_DEP="-I/Program Files/vcpkg/installed/x64-windows/include" "-L/Program Files/vcpkg/installed/x64-windows/bin" "-L/Program Files/vcpkg/installed/x64-windows/lib"
 set RAYLIB_DEP=-lraylib -lopengl32 -lgdi32 -lwinmm
 set HPDF_DEP=-lhpdf -lzlib -llibpng16
-set DEP=%VCPKG_DEP% %RAYLIB_DEP% %HPDF_DEP%
+set DEP=-I./deps/stb -I./deps/tsoding -I./deps/QuelSolaar %VCPKG_DEP% %RAYLIB_DEP% %HPDF_DEP%
 
 cmd /c if not exist bin\assets\ xcopy assets\ bin\assets\ /E
 cmd /c if exist bin\rl.exe del /F bin\rl.exe
