853514531426aed8e04774cabbbf186506d8e397
ArtInLines <linesinart@gmail.com>
2023-10-03 12:43:14 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,12 @@
+:: Run "run.bat r" to remove the data directory before running rl.exe
+
 @echo off
 call build.bat
-cd bin
-call rl.exe
-cd ..
+if %errorLevel%==0 (
+	cd bin
+	if "%~1"=="r" (
+		del /Q data
+	)
+	call rl.exe
+	cd ..
+)
