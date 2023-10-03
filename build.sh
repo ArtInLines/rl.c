475ee87c1214274c18a95773b7390e4fe87514fb
ArtInLines <linesinart@gmail.com>
2023-10-03 13:00:25 +0200

    Added todo comment

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ DEV_FLAGS="-g -ggdb"
 CFLAGS="-Wall -Wextra -Wimplicit -Wpedantic -Wno-unused-function -std=c99"
 
 # Compiler-Options to include dependencies
+# @TODO: The dependencies have to be different for unix systems of course. It would be better, if the script was able to build all dependencies from source anyways, as no other setup would be required then.
+# This is a low priority issue though
 VCPKG_DEP="\"-I/Program Files/vcpkg/installed/x64-windows/include\" \"-L/Program Files/vcpkg/installed/x64-windows/bin\" \"-L/Program Files/vcpkg/installed/x64-windows/lib\""
 RAYLIB_DEP="-lraylib -lopengl32 -lgdi32 -lwinmm"
 HPDF_DEP="-lhpdf -lzlib -llibpng16"
